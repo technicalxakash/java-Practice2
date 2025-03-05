edce5928ec53031585108f237321934e08537c6e
technicalxakash <akashr2468@gmail.com>
2025-03-05 13:00:26 +0530

    scanner

--- basic.java
+++ basic.java
@@ -224,7 +224,7 @@ public class localVariables {
     	
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		localVariables obj=new localVariables();
 				obj.add();
 				obj.sub(10, 5);
@@ -400,4 +400,39 @@ public class Program2 {
        System.out.println(b);   //expilicit type converstion
 
 
+
+Scanner Class
+import java.util.Scanner;
+
+public class Scannerr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the byte value :");
+		byte bt=sc.nextByte();
+		System.out.println("the byte value you entered is "+bt);
+		System.out.println("enter the short value :");
+		short sh=sc.nextShort();
+		System.out.println("the short value you entered is "+sh);
+		System.out.println("enter the integer value :");
+		int it=sc.nextInt();
+		System.out.println("the integer value you entered is "+it);
+		System.out.println("enter the long  value :");
+		long lg=sc.nextLong();
+		System.out.println("the long value you entered is "+lg);
+		System.out.println("enter the float value :");
+		float ft=sc.nextFloat();
+		System.out.println("the float value you entered is "+ft);
+		System.out.println("enter the string value :");
+		String str=sc.next();
+		System.out.println("the string value you entered is "+str);
+		System.out.println("enter the string value :");
+		String str1=sc.nextLine();
+		System.out.println("the string value you entered is "+str1);
+
+	}
+
+}
 */
+
