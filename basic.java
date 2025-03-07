ad4f98426bb1f863710dea663e40b58759be0a46
technicalxakash <akashr2468@gmail.com>
2025-03-07 19:23:45 +0530

    conditional statements and conditional operators

--- basic.java
+++ basic.java
@@ -630,6 +630,80 @@ System.out.println("the num2 value you entered is "+sc.nextInt());
 
 }
 
+conditional statements
+import java.util.Scanner;
+
+public class conditionalStatements {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number 1 : ");
+		int num1=sc.nextInt();
+		if(num1>=30) {
+			System.out.println("num1 is greater");
+		}
+		if(num1<=30) {
+			System.out.println("num1 is lesser");
+		}
+		if(num1==30) {
+			System.out.println("num1 is equal to 30");
+		}
+		else {
+			System.out.println("hii");
+		}
+		
+		System.out.println("enter the number 1 : ");
+		int num1=sc.nextInt();
+		
+		if(num1==20) System.out.println("num1 is equal to 20");
+		else if(num1<20)System.out.println("num1 is lesser than to 20");
+		else System.out.println("num1 is greater than to 20");
+       
+	}
+
+}
+conditional operators
+
+import java.util.Scanner;
+
+public class ConditionalOperator {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number 1 : ");
+		int num1=sc.nextInt();
+		System.out.println("enter the number 2 : ");
+		int num2=sc.nextInt();
+		System.out.println("enter the number 3 : ");
+		int num3=sc.nextInt();
+		if(num1>num2 && num1>num3){
+			System.out.println("num1 is greater");
+		}
+		else if(num2>num1 && num2>num3){
+			System.out.println("num2 is greater");
+		}
+			else {
+				System.out.println("num3 is greater");
+			}
+		
+	   System.out.println("============================");
+		
+	   System.out.println("enter the passcode : ");
+	   String name=sc.next();
+	   System.out.println("enter the key : ");
+	   int key=sc.nextInt();
+	   if (name.equals("akash")||key==10110) {
+		System.out.println("entry permitted");   
+	   }
+	   else {
+		   System.out.println("Not permitted");  
+	   }
+	   
+	   
+
+	}
+
+}
 
 */
 
