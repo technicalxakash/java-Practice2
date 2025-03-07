e2f70d80bd3b339e4839213ab1a44c7cd496dfb6
technicalxakash <akashr2468@gmail.com>
2025-03-07 19:42:55 +0530

    different type of print statements

--- basic.java
+++ basic.java
@@ -620,7 +620,7 @@ import java.util.Scanner;
 public class Identifiers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter the value of num1 : ");
 System.out.println("the num1 value you entered is "+sc.nextInt());
@@ -704,6 +704,31 @@ public class ConditionalOperator {
 	}
 
 }
+different type of printing statements
+package www.akash.home;
+
+public class PrintStatements {
+
+	public static void main(String[] args) {
+		System.out.print("Hello akash");
+		System.out.print("How are you");
+		System.out.println("Hello akash");
+		System.out.println("How are you");
+		// printf,println.print are methods
+		System.out.print("How are you");
+		System.out.println("Hello akash");
+	System.out.print("How are you");
+		
+		//printf
+		System.out.printf("the year akash born in %d and the month is %s",2003,"July");
+		//err
+		System.err.println("Hello akash");
+		System.err.print("How are you");
+		System.err.printf("the year akash born in %d and the month is %s",2003,"July");
+	}
+
+}
+
 
 */
 
