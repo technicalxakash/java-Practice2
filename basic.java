c61bd9ccc31fa348019b09aaf34b9642bdf20054
technicalxakash <akashr2468@gmail.com>
2025-03-07 14:36:51 +0530

    arthematic operators and identifiers

--- basic.java
+++ basic.java
@@ -581,5 +581,55 @@ great
 hello
 great
 
+
+Arthematic operators
+
+import java.util.Scanner;
+
+public class ArthematicOpertor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("enter second number: ");
+        int num2 = sc.nextInt();
+        int res1 = num1+num2;
+        System.out.println("Addition result is:"+res1);
+        System.out.println("=========================");
+        int res2 = num1-num2;
+        System.out.println("Substraction  result is:"+res2);
+        System.out.println("=========================");
+        int res3 = num1*num2;
+        System.out.println("multiplication  result is:"+res3);
+        System.out.println("=========================");
+        int res4= num1/num2;
+        System.out.println("division  result is:"+res4);
+        System.out.println("=========================");
+        int res5 = num1+num2;
+        System.out.println("modulus  result is:"+res5);
+        System.out.println("=========================");
+    }
+}
+
+
+
+identifiers
+
+import java.util.Scanner;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of num1 : ");
+System.out.println("the num1 value you entered is "+sc.nextInt());
+System.out.println("Enter the value of num2 : ");
+System.out.println("the num2 value you entered is "+sc.nextInt());
+	}
+
+}
+
+
 */
 
