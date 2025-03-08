0b0400753e458e2d6b89b73e61d7bd2397e2b606
technicalxakash <akashr2468@gmail.com>
2025-03-09 04:19:22 +0530

    exmaplez of while and do-while

--- basic.java
+++ basic.java
@@ -895,8 +895,134 @@ Decision making statements problem
 		    }
 		}
 
+//looping statements exmaplez of while and do-while
+
+package www.akash.home;
+
+import java.util.Scanner;
+
+public class loopps {
+
+	public static void main(String[] args) {
+		//while loops
+		//		int i=0;
+		//		while(i<=10) {
+		//			
+		//			while(j<=10) {}
+		//			System.out.println("3*"+i+"="+i*3);
+		//			i=i+1;
+		//		}
+
+		//		int i=0;
+		//		while(i<10) {
+		//			System.out.println(i);
+		//			i++;
+		//		}
+
+		//do while
+		//		int i=1;
+		//		do {
+		//			System.out.println(i);
+		//			i++;
+		//		}while(i<=0);
+
+
+		//1)print 1 to 100 values
+		//while
+		//		int i=1;
+		//		while(i<=100) {
+		//			System.out.println(i);
+		//			i++;
+		//		}
+		//do while
+		//		int i=1;
+		//		do {
+		//			System.out.println(i);
+		//			i++;
+		//		}
+		//		while(i<=100);
+
+
+
+		//2)print even numbers between 200 and 500
+		//using while
+		//		int i=200;
+		//		while(i<=500) {
+		//			System.out.println(i);
+		//			i=i+1;
+		//			i++;			
+		//		}
+
+		//using do while 
+		//		int i=200;
+		//		do {
+		//			System.out.println(i);
+		//			i=i+1;
+		//			i++;	
+		//			
+		//		}
+		//		while(i<=500);
+
+
+		//3)print the numbers which is divisible by 7 for the range of 150 to 200
+		//while loop
+		//	int i=150;
+		//	
+		//		while(i<=200) {
+		//			if(i%7==0) {
+		//			System.out.println(i);
+		//			
+		//		}
+		//			i++;
+		//		}
+		//or
+		//		int i=154;
+		//		while(i<=200) {
+		//			System.out.println(i);
+		//			i+=7;
+		//		}
+
+
+		//5)print the sum of the even numbers between 40 to 80
+		//		Scanner sc=new Scanner(System.in);
+		//		System.out.println("enter the FirstNumber : ");
+		//		int FirstNumber=sc.nextInt();
+		//		System.out.println("enter the LastNumber : ");
+		//		int LastNumber=sc.nextInt();
+		//		while(FirstNumber<=LastNumber) {
+		//			if(FirstNumber%2==0) {
+		//				System.out.println(FirstNumber);
+		//			}
+		//			
+		//			FirstNumber++;
+		//		}
+		//		6)Print the odd numbers between 200 to 25(reverse order)
+		//		Scanner sc=new Scanner(System.in);
+		//		System.out.println("enter the FirstNumber : ");
+		//		int FirstNumber=sc.nextInt();
+		//		System.out.println("enter the LastNumber : ");
+		//		int LastNumber=sc.nextInt();
+		//		while(FirstNumber>=LastNumber) {
+		//			if(FirstNumber%2!=0) {
+		//				System.out.println(FirstNumber);
+		//			}
+		//			
+		//			FirstNumber--;
+		//		}
+
+		//4)prime number or not
+
+
+	}
+
+}
+
+
+
 
 
 
 */
+
+
 
