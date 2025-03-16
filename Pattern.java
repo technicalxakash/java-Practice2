b72ebac2e5ed41aa5f81cb682755562a068f7201
technicalxakash <akashr2468@gmail.com>
2025-03-16 20:51:56 +0530

    array problem

--- Pattern.java
+++ Pattern.java
@@ -2258,6 +2258,258 @@ public static void main(String[] args) {
 	}
 }}
 
+
+
+package www.akash.home;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+  
+public static void main(String[] args) { 
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the size of an array : ");
+	int n=sc.nextInt();
+	
+	int a[]= new int[n];
+	System.out.println("enter the elemsts of an array");
+	
+	for(int i=0;i<n;i++) {
+		System.out.print("enter the element at "+(i+1)+" :");
+		 a[i]=sc.nextInt();
+	}
+	int sum=0;
+	
+	
+	
+	for(int i=0;i<a.length;i++) {
+		sum=sum+a[i];
+	}
+	System.out.println("the sum of the array is "+sum);
+	//arrays.toString()=to print in a arrays form
+	System.out.println(Arrays.toString(a));
+	
+	
+}}
+
+
+
+//joining two array into single arrays
+package www.akash.home;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+  
+public static void main(String[] args) { 
+	Scanner sc=new Scanner(System.in);
+	
+	
+	System.out.println("enter the size of an array1 : ");
+	int n1=sc.nextInt();
+	String[] arr1=new String[n1];
+	for(int i=0;i<n1;i++) {
+		System.out.println("enter the arr1 of index of  "+(i+1)+" : ");
+		arr1[i]=sc.next();
+	}
+	
+	
+	System.out.println("enter the size of an array2 : ");
+	int n2=sc.nextInt();	
+	String[] arr2=new String[n2];	
+	for(int i=0;i<n2;i++) {
+		System.out.println("enter the arr1 of index of "+(i+1)+" : ");
+		arr2[i]=sc.next();
+	}
+	
+	
+	System.out.println(Arrays.toString(arr1));
+	System.out.println(Arrays.toString(arr2));
+	System.out.println("after joing arr1 and arr2 with arr3");
+	
+	String[] arr3=new String[arr1.length+arr2.length];
+	for(int i=0;i<arr1.length;i++) {
+		arr3[i]=arr1[i];
+	}
+	for(int i=0;i<arr2.length;i++) {
+		arr3[i+arr1.length]=arr2[i];
+	}
+	System.out.println(Arrays.toString(arr3));
+	
+}}
+
+//largest number in given array
+package www.akash.home;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+  
+public static void main(String[] args) { 
+	Scanner sc=new Scanner(System.in);
+	
+	
+	System.out.println("enter the size of an array : ");
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	System.out.println("enter the arrays");
+	for(int i=0;i<a.length;i++) {
+		System.out.println("enter the arr1 of index of "+(i+1)+" : ");
+		a[i]=sc.nextInt();
+	}
+	System.out.println(Arrays.toString(a));
+	int largestnum=a[0];
+	int maxIndex=0;
+	int secondLargest=a[0];
+	int secondMaxIndex=0;
+	for(int i=0;i<a.length;i++) {
+		if(largestnum<a[i]) {
+			largestnum=a[i];
+			maxIndex=i;
+		}
+	
+    }
+		
+	}System.out.println("the largest value in given array is :  "+largestnum);
+	System.out.println("the largest value index  in given array is : "+maxIndex);
+	
+}}
+
+
+package www.akash.home;
+import java.util.Arrays;
+
+
+public class Array {
+  
+public static void main(String[] args) { 
+	
+	int[] a=new int[] {2,4,23,65,76};
+	int max=a[0];
+	int secondMax=a[1];
+	  for (int i = 0; i < a.length; i++) {
+          if (a[i] > max) {
+              secondMax = max; // Update second largest before replacing largest
+              max = a[i];
+          } else if (a[i] > secondMax && a[i] != max) {
+              secondMax = a[i];
+          }
+      }
+	System.out.println("second maximum elemets is "+secondMax);
+	
+}
+}
+
+//largest number in given array
+package www.akash.home;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array{
+  
+public static void main(String[] args) { 
+		
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the size of an array : ");
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	System.out.println("enter the arrays");
+	for(int i=0;i<a.length;i++) 
+	{
+		System.out.println("enter the arr1 of index of "+(i+1)+" : ");
+		a[i]=sc.nextInt();
+	}
+	System.out.println(Arrays.toString(a));
+	int largestnum=a[0];
+	int maxIndex=0;
+
+	for(int i=0;i<a.length;i++) {
+		if(largestnum<a[i]) {
+			largestnum=a[i];
+			maxIndex=i;
+		}
+	
+    }
+	System.out.println("the largest value in given array is :  "+largestnum);
+	System.out.println("the largest value index  in given array is : "+maxIndex);
+	}
+
+	
+}
+*/
+/* 
+//second largest number 
+package www.akash.home;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array{
+  
+public static void main(String[] args) { 
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the size of an array : ");
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	System.out.println("enter the arrays");
+	for(int i=0;i<a.length;i++) 
+	{
+		System.out.println("enter the arr1 of index of "+(i+1)+" : ");
+		a[i]=sc.nextInt();
+	}
+	System.out.println(Arrays.toString(a));
+	int max1=0;
+	int max2=0;
+	int maxIndex=0;
+	int maxIndex2=0;
+	
+	if(a[0]>a[1]) {
+		max1=a[0];
+		max2=a[1];
+	}
+	else {
+		max1=a[1];
+		max2=a[0];
+	}
+	for(int i=2;i<a.length;i++) {
+		if(a[i]>max1) {
+			max2=max1;
+			max1=a[i];
+			maxIndex=i;
+			
+		}
+		else if(a[i]>max2){
+			
+			max2=a[i];
+			maxIndex2=i;
+		}
+	}
+	System.out.println("the firstlargest element is"+max1);
+	System.out.println("the secondlargest element is"+max2);
+	System.out.println("the largest value index  in given array is : "+maxIndex);
+	System.out.println("the Secondlargest value index  in given array is : "+maxIndex2);
+}}
+
+output=enter the size of an array : 
+5
+enter the arrays
+enter the arr1 of index of 1 : 
+32
+enter the arr1 of index of 2 : 
+545
+enter the arr1 of index of 3 : 
+23
+enter the arr1 of index of 4 : 
+56
+enter the arr1 of index of 5 : 
+45
+[32, 545, 23, 56, 45]
+the firstlargest element is545
+the secondlargest element is56
+the largest value index  in given array is : 0
+the Secondlargest value index  in given array is : 3
+
 */
 
 
