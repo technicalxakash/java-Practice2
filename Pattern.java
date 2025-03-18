fe3b56116d630533b5e7f49033d90135db41e4e6
technicalxakash <akashr2468@gmail.com>
2025-03-18 21:45:39 +0530

    palindrome and rev

--- Pattern.java
+++ Pattern.java
@@ -2622,6 +2622,55 @@ enter the element at 5 :67845
 enter the element at 6 :43
 sorted array is 
 [1, 23, 34, 43, 3234, 67845]
+
+
+
+
+
+pallindrom
+package www.akash.home;
+
+public class pallindrom {
+
+	public static void main(String[] args) {
+		String str="acecar";
+		char[] arr=str.toCharArray(); //The toCharArray() method in Java converts a string into a character array, 
+		String rev="";
+		for(int i=arr.length-1;i>=0;i--) {
+			rev=rev+arr[i];
+		}
+		if(str.equals(rev)) {
+		System.out.println("it is palidrome");
+		}
+		else {
+			System.out.println("it is not palidrome");
+	
+		}
+
+	}
+
+		}
+
+
+string reverse
+package www.akash.home;
+  
+ public class RevString {
+ 
+ public static void main(String[] args) 
+ { 
+	 String str="racecar"; char[]
+ arr=str.toCharArray(); //The toCharArray() method in Java converts a string into a character array, 
+ String rev=""; 
+ for(int i=arr.length-1;i>=0;i--) {
+rev=rev+arr[i];
+} 
+ System.out.println(rev);
+ 
+ }
+ 
+ }
+
 */
 
 
