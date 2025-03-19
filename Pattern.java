a10cf0f5e4378e95bd5916c3b65828f6fdaae03f
technicalxakash <akashr2468@gmail.com>
2025-03-19 06:59:26 +0530

    panagram

--- Pattern.java
+++ Pattern.java
@@ -2671,6 +2671,54 @@ rev=rev+arr[i];
  
  }
 
+
+ panagram
+ package www.akash.home;
+
+public class panagram {
+
+	public static void main(String[] args) {
+	 String str="the quick brown fox jumps over the lazy dog";
+	 str=str.toLowerCase();
+	boolean ispanagram=true;
+     for(char ch='a';ch<='z';ch++) {
+    	 if(!str.contains(String.valueOf(ch))){  //String.valueOf(ch) converts the character into a string
+    		 ispanagram=false;
+    		 break;
+    		 
+    	 }
+    	 
+     }
+     if (ispanagram) System.out.println("It is a pangram.");
+         
+      else {
+         System.out.println("It is not a pangram.");
+     }
+
+     
+	}
+
+}
+simple 
+
+
+package www.akash.home;
+
+public class panagram {
+
+	public static void main(String[] args) {
+	 String str="the quick brown fox jumps over the lazy dog";	 
+	
+	 for(char ch='a';ch<='z';ch++) {
+		 if(!str.contains(String.valueOf(ch))) {
+			 System.out.println("it is not panagram");
+			 break;
+		 }
+		 
+	 }
+	 System.out.println("it is panagram");
+	}}
+
 */
 
 
