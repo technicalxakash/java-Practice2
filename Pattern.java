6940e93f86885c41d0ae06ff3654ae29ee3f951a
technicalxakash <akashr2468@gmail.com>
2025-03-20 23:00:34 +0530

    StringBuiltInMethods

--- Pattern.java
+++ Pattern.java
@@ -2848,6 +2848,190 @@ public class patternNew {
 }
 }
 }
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		/*
+//literal based representations =equals
+				String s1="akash";
+				String s2="akash";
+				if(s1.equals(s2)) {
+					System.out.println("s1 equals to s2");
+				}
+				else {
+					System.out.println("s1 is not equals s2");
+				}
+				if(s1==s2) {
+					System.out.
+					println("address is equals");	
+				}
+				else {
+					System.out.println("address are not equals");	
+				}
+		 //output=s1 equals to s2
+				// address is equals
+
+//object based representation =equals
+				String s1=new String("Akash");
+				String s2=new String("Akash");
+				if(s1.equals(s2)) {
+					System.out.println("s1 equals to s2");
+				}
+				else {
+					System.out.println("s1 is not equals s2 ");
+				}
+				if(s1==s2) {
+					System.out.println("address is equals");	
+				}
+				else {
+					System.out.println("address are not equals");	
+				}
+          output=s1 equals to s2
+        		  address are not equals
+
+
+//literal based representations =equalsIgnoreCase
+		String s1="akash";
+		String s2="AKASH";
+		if(s1.equalsIgnoreCase(s2)) {
+			System.out.println("s1 equals to s2");
+		}
+		else {
+			System.out.println("s1 is not equals s2");
+		}
+		if(s1==s2) {
+			System.out.
+			println("address is equals");	
+		}
+		else {
+			System.out.println("address are not equals");	
+		}
+
+		output=s1 equals to s2
+				address are not equals
+
+
+//object based representation =equalsIgnoreCase
+		String s1=new String("Akash");
+		String s2=new String("akash");
+		if(s1.equalsIgnoreCase(s2)) {
+			System.out.println("s1 equals to s2");
+		}
+		else {
+			System.out.println("s1 is not equals s2 ");
+		}
+		if(s1==s2) {
+			System.out.println("address is equals");	
+		}
+		else {
+			System.out.println("address are not equals");	
+		}
+		output=s1 equals to s2
+				address are not equals
+				 
+		
+//compareTo
+		String s1=new String("Akash");
+		String s2=new String("Akash");
+		if(s1.compareTo(s2)==0) {
+			System.out.println("it is a same");
+		}
+		else {
+			System.out.println("it is not same");
+		}
+		
+//	.compareToIgnoreCase
+		String s1=new String("Akash");
+		String s2=new String("akash");
+		if(s1.compareToIgnoreCase(s2)==0) {
+			System.out.println("it is a same");
+		}
+		else {
+			System.out.println("it is not same");
+			
+		}
+		
+		
+//compareTo with memory difference
+				String s1=new String("Akash");
+				String s2=new String("akash");
+				if(s1.compareTo(s2)==0) {
+					System.out.println("it is a same");
+				}
+				else {
+					System.out.println("it is not same");
+				}
+		
+		System.out.println(s1.compareTo(s2));
+		output=it is notsame
+                -32
+		
+//compareToIgnoreCase with memory difference
+		String s1=new String("Akash");
+		String s2=new String("akash");
+		if(s1.compareToIgnoreCase(s2)==0) {
+			System.out.println("it is a same");
+		}
+		else {
+			System.out.println("it is not same");
+			
+		}
+		System.out.println(s1.compareTo(s2));
+		output=it is a same
+                -32
+
+		
+		
+		
+//charAt
+		    String s1 = "akashAkashAKash";
+	       System.out.println(s1.charAt(9)); //output = h
+	       System.out.println(s1.charAt(90)); //output =Index 90 out of bounds for length 15
+				int i=s1.charAt(9);
+				System.out.println(i);
+				
+System.out.println("=======================================");
+				
+//indexOf3
+          String s1 ="SachinRameshGandhi";
+          System.out.println(s1.indexOf("a"));//1
+          System.out.println(s1.indexOf("Ramesh"));//6
+          System.out.println("=======================================");
+          
+		
+//contains
+		String s1 ="SachinRameshGandhi";
+		System.out.println(s1.contains("Ramesh"));//true
+		System.out.println(s1.contains("Uamesh"));//false
+		System.out.println(s1.contains("RAM"));//false
+		System.out.println("=======================================");
+
+//endsWith
+		String s1 ="SachinRameshGandhi";
+		boolean res=s1.endsWith("Gandhi");//true
+		System.out.println(res);      
+		System.out.println(s1.endsWith("Hi"));//false
+		System.out.println(s1.endsWith("i"));//true
+
+//hashCode
+		String s1 ="SachinRameshGandhi";
+		System.out.println(s1.hashCode()); //output=returns a hash value of an objects,850348197
+	
+//getClass
+		String s1 ="SachinRameshGandhi";
+		System.out.println(s1.getClass());
+		//output=class java.lang.String
+		*/
+	}
+
+
+}
+		
+	
+
+
+
 		 */
 	
 
