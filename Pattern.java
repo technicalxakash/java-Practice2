d27ea401d63865d2fa17f48b7a59cb5fcc2b8781
technicalxakash <akashr2468@gmail.com>
2025-03-22 13:54:21 +0530

    2D and jagged array

--- Pattern.java
+++ Pattern.java
@@ -3071,6 +3071,119 @@ System.out.println();
 
 
 
+package www.akash.home;
+
+public class Array {
+ 
+public static void main(String[] args) {
+//2D array	 
+	int arr[][]=new int[3][2];
+	arr[0][0]=1;
+	arr[0][1]=2;
+	
+	arr[1][0]=1;
+	arr[1][1]=2;
+	
+	arr[2][0]=1;
+	arr[2][1]=2;
+	
+	
+	for(int i=0;i<arr.length;i++) {
+		for(int j=0;j<arr[i].length;j++) {
+			System.out.print(arr[i][j]+" ");
+		}
+		System.out.println();
+	}
+	
+System.out.println("==============================");
+//or	
+
+
+	int arr1[][]= {
+			{1,2},
+			{3,4},
+			{5,6}
+	};
+	int size=0;
+	for(int i=0;i<arr1.length;i++) {
+		for(int j=0;j<arr1[i].length;j++) {
+			System.out.print(arr1[i][j]+" ");
+			size+=1;
+		}
+		System.out.println();
+	
+		
+		
+	}
+	System.out.println(size);
+ }
+ 
+ }
+
+
+
+package www.akash.home;
+
+public class Array {
+ 
+public static void main(String[] args) {
+	
+//jagged array	 
+		int arr[][]=new int[3][];
+		
+		 arr[0] = new int[3];  
+	     arr[1] = new int[5];  
+	     arr[2] = new int[2];
+	     
+	     
+		arr[0][0]=1;
+		arr[0][1]=2;
+		arr[0][2]=4;
+		
+		arr[1][0]=1;
+		arr[1][1]=2;
+		arr[1][2]=2;
+		arr[1][3]=2;
+		arr[1][4]=2;
+		
+		arr[2][0]=1;
+		arr[2][1]=2;
+		int size=0;
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+" ");
+				size+=1;
+			}
+			System.out.println();
+		
+			
+			
+		}
+		System.out.println(size);
+		
+		System.out.println("===================================="); 
+		//or
+		int arr1[][]= {
+				{1,2},
+				{3,4,45,2,452,5},
+				{5,6,6,5}
+		};
+		int sizee=0;
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr1[i].length;j++) {
+				System.out.print(arr1[i][j]+" ");
+				sizee+=1;
+			}
+			System.out.println();
+		
+			
+			
+		}
+		System.out.println(sizee);
+	 }
+	 
+	 
+}
 
 
 
