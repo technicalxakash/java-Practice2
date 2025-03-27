4c466a3112dbe7ec09a7ef65301c03a2a334b994
technicalxakash <akashr2468@gmail.com>
2025-03-27 22:13:14 +0530

    String built in methods

--- Pattern.java
+++ Pattern.java
@@ -3715,6 +3715,168 @@ tryui dfghj gjhkl      hgvjbknlkm
 Total number of words is 4
 
 
+
+//9)Count the number of charcters,numbers and special chaarter
+package www.com.program.string;
+import java.util.Scanner;
+
+public class Program9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+
+        String alphabets = "";
+        String numbers = "";
+        String specialChars = "";
+        int sumOfalphabets=0;
+        int sumOfnumbers=0;
+        int sumOfspecialChars=0;
+
+        for(int i=0;i<str.length();i++) {
+        	
+        	if(str.charAt(i)>='A' && str.charAt(i)>='Z' || str.charAt(i)>='a' && str.charAt(i)>='z') {
+        		
+        		alphabets=alphabets+(char) (str.charAt(i));
+        		sumOfalphabets=sumOfalphabets+str.charAt(i);
+        		
+        	}
+        	else if(str.charAt(i)>='0' && str.charAt(i)<='9') {
+        		numbers=numbers+(char)(str.charAt(i));
+        		sumOfnumbers=sumOfnumbers+str.charAt(i);
+        		
+        	}
+        	else {
+        		specialChars=specialChars+(char)(str.charAt(i));
+        		sumOfspecialChars=sumOfspecialChars+str.charAt(i);
+        	}
+        }
+       
+
+        System.out.println("Alphabets: " + alphabets);
+        System.out.println("SumOfalphabets : "+sumOfalphabets);
+        System.out.println("===============================");
+        System.out.println("Numbers: " + numbers);
+        System.out.println("sumOfnumbers : "+sumOfnumbers);
+        System.out.println("===============================");
+        System.out.println("specialChars : "+specialChars);
+        System.out.println("sumOfspecialChars : " +sumOfspecialChars);
+        System.out.println("===============================");
+        
+
+        sc.close();
+    }
+}
+
+
+sdfg3214@!#$%
+Alphabets: sdfg
+SumOfalphabets : 420
+===============================
+Numbers: 3214
+sumOfnumbers : 202
+===============================
+specialChars : @!#$%
+sumOfspecialChars : 205
+===============================
+
+
+
+10)anagram
+
+package www.com.program.string;
+
+import java.util.Scanner;
+
+public class Program10 {
+
+	
+	public static char[] bubbleSort(char a[]) {
+		for(int i=0;i<a.length-1;i++) {
+			for(int j=i+1;j<a.length;j++) {
+			if(a[i]>a[j]) {
+			  char temp=a[i];
+			    a[i]=a[j];
+			    a[j]=temp;
+			    
+			}
+			}
+			
+		}
+		return a;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("enter the string : ");
+		String str1=sc.nextLine();
+		
+		System.out.print("enter the string : ");
+		String str2=sc.nextLine();
+		
+		if(str1.length()==str2.length()) {
+			char arr1[] =new char[str1.length()];
+			char arr2[]=new char[str2.length()];
+			
+			for(int i=0;i<str1.length();i++) {
+				
+				arr1[i]=str1.charAt(i);
+				arr2[i]=str2.charAt(i);
+			}
+
+			arr1=bubbleSort(arr1);
+			arr2=bubbleSort(arr2);
+
+			
+			for(int i=0;i<arr1.length;i++) {
+				if(arr1[i]!=arr2[i]) {
+					System.out.println("Not anagram");
+					System.exit(0);
+				}
+			}
+			System.out.println("anagram");
+			}
+			
+		}
+	}
+
+
+package www.com.program.string;
+
+import java.util.Arrays;
+
+
+public class Program10 {
+	public static void main(String[] args) {
+		
+		String str1="aasss";
+		String str2="aass";
+		
+		if(str1.length()==str2.length()) {
+		char[] str11=str1.toCharArray();
+		char[] str12=str2.toCharArray();
+		
+		 
+		Arrays.sort(str11);
+		Arrays.sort(str12);
+		
+
+		
+		 if (Arrays.equals(str11, str12)) {
+             System.out.println("It is an anagram");
+             System.exit(0);
+         } 
+		 
+		
+		 
+		}
+		System.out.println("It is not an anagram");
+		
+		
+	}
+		}
+
 	*/
 
 
