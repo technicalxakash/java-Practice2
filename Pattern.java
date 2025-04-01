a9231cd87428079f7f0e8e7a6a21235486b0453f
technicalxakash <akashr2468@gmail.com>
2025-04-01 20:49:43 +0530

    Using StringBuffer and String Builder

--- Pattern.java
+++ Pattern.java
@@ -4230,7 +4230,52 @@ output=40
 		
         
 
+public class StringBuffAndBuild {
 
+	public static void main(String[] args) {
+	
+		String s1="rama";
+		System.out.println(s1);
+		s1.concat("seetha");
+		System.out.println(s1);
+		
+		//rama
+		//rama
+	
+		String s1="rama";
+		System.out.println(s1);
+		String s2=s1.concat("seetha");
+		System.out.println(s2);
+
+		//rama
+		//ramaseetha
+	
+		
+		
+		//Using StringBuffer
+		StringBuffer s1=new StringBuffer("Rama");
+		System.out.println(s1);
+		s1.append("Seetha");
+		System.out.println(s1);
+		
+		Rama
+        RamaSeetha
+		
+		
+		//Using  String Builder
+		
+		StringBuilder s1=new StringBuilder("Rama");
+		System.out.println(s1);
+		s1.append("tata");
+		System.out.println(s1);
+		
+		Rama
+		Ramatata
+		
+	
+	}
+
+}
 
 		Java - What is OOP?
 OOP stands for Object-Oriented Programming.
