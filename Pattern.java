4bf3a6dd7b8835d1b46613790b788570b467f382
technicalxakash <akashr2468@gmail.com>
2025-04-01 22:15:03 +0530

    polymorphism = method overloading,compile time polymorphism

--- Pattern.java
+++ Pattern.java
@@ -4277,6 +4277,57 @@ public class StringBuffAndBuild {
 
 }
 
+
+
+polymorphism
+1)compilation time polymorphism is achieved by method overloading
+
+
+package javaa.oops.com;
+
+
+class Calculator{
+	
+	int add(int a, int b) {
+		return a+b;
+	}
+	
+	int add(int a ,float b) {
+		return (int)(a+b);
+	}
+	
+	float add(float a, float b ,float c) {
+		return a+b+c;
+	}
+}
+
+public class MethodOverloadingPolyCompil {
+
+	public static void main(String[] args) {
+	
+		Calculator cal=new Calculator();
+		int a=10;
+		int b=13;
+		float aa=20;
+		float bb=30;
+		float cc=80;
+		System.out.println(cal.add(a, b));
+		System.out.println(cal.add(a, bb));
+		System.out.println(cal.add(aa, bb, cc));
+
+	}
+
+}
+
+23
+40
+130.0
+
+
+
+
+
+
 		Java - What is OOP?
 OOP stands for Object-Oriented Programming.
 
