199cd173efa54d942cfb2096eb3cb86b18f7eee3
technicalxakash <akashr2468@gmail.com>
2025-04-03 23:54:29 +0530

    constructor

--- Pattern.java
+++ Pattern.java
@@ -4593,6 +4593,150 @@ output=this is n in a
 this is n in b
 
 
+constructor
+package www.akash.home;
+class Dogg{
+    private String name;
+	private String color;
+	private String breed;
+	private int age;
+	private int cost;
+	
+	public Dogg() {
+		this("Jimmy");
+		System.out.println("inside zero paramterized constructor");
+	this.name="Tommy";
+	this.color="red";
+	this.breed="pug";
+	this.age=2;
+	this.cost=4000;
+	System.out.println(this.name);
+	System.out.println(this.color);
+	System.out.println(this.breed);
+	System.out.println(this.age);
+	System.out.println(this.cost);
+		
+	}
+	
+	public Dogg (String name){
+		this("Jimmy","Brown");
+		System.out.println("inside one paramterized constructor");
+		this.name=name;
+		System.out.println(this.name);
+		System.out.println(this.color);
+		System.out.println(this.breed);
+		System.out.println(this.age);
+		System.out.println(this.cost);
+		
+	}
+	public Dogg (String name,String color ){
+		this("Jipey","white","GR");
+		System.out.println("inside two paramterized constructor");
+		this.name=name;
+		this.color=color;
+		System.out.println(this.name);
+		System.out.println(this.color);
+		System.out.println(this.breed);
+		System.out.println(this.age);
+		System.out.println(this.cost);
+	
+}
+	public Dogg (String name,String color ,String breed ){
+		this("chintu","purple","Shitzu",3);
+		System.out.println("inside three paramterized constructor");
+		this.name=name;
+		this.color=color;
+		this.breed=breed;
+		System.out.println(this.name);
+		System.out.println(this.color);
+		System.out.println(this.breed);
+		System.out.println(this.age);
+		System.out.println(this.cost);
+
+}
+	public Dogg (String name,String color ,String breed,int age ){
+		
+		this("rocky","brown-black","german shephard",7,9000);
+		System.out.println("inside four paramterized constructor");
+		this.name=name;
+		this.color=color;
+		this.breed=breed;
+		this.age=age;
+		System.out.println(this.name);
+		System.out.println(this.color);
+		System.out.println(this.breed);
+		System.out.println(this.age);
+		System.out.println(this.cost);
+
+}
+	public Dogg (String name,String color ,String breed,int age ,int cost){
+		
+		System.out.println("inside five paramterized constructor");
+		this.name=name;
+		this.color=color;
+		this.breed=breed;
+		this.age=age;
+		this.cost=cost;
+		System.out.println(this.name);
+		System.out.println(this.color);
+		System.out.println(this.breed);
+		System.out.println(this.age);
+		System.out.println(this.cost);
+
+}
+}
+public class ConstructorsDest {
+
+	public static void main(String[] args) {
+	
+		Dogg  d=new Dogg();
+		
+	}
+
+}
+output=
+inside five paramterized constructor
+rocky
+brown-black
+german shephard
+7
+9000
+inside four paramterized constructor
+chintu
+purple
+Shitzu
+3
+9000
+inside three paramterized constructor
+Jipey
+white
+GR
+3
+9000
+inside two paramterized constructor
+Jimmy
+Brown
+GR
+3
+9000
+inside one paramterized constructor
+Jimmy
+Brown
+GR
+3
+9000
+inside zero paramterized constructor
+Tommy
+red
+pug
+2
+4000
+
+
+
+
+
+
 
 
 		Java - What is OOP?
@@ -4717,6 +4861,10 @@ admin can read
  he can read 
  he can write
  he can manage
+
+
+ 2)
+
         */
 		
 		
